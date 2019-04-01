4cb9c98b76d651c40ded4b85337cae4754e204c8
hugoferrari <dariohugoferrari@gmail.com>
2019-04-01 20:32:48 -0300

    primerrrr

---
+++ interrupcion.ino
@@ -0,0 +1,24 @@
+volatile int contador = 0;   // Somos de lo mas obedientes
+int n = contador ;
+long T0 = 0 ;  // Variable global para tiempo
+
+void setup()
+   {    pinMode(2, INPUT_PULLUP);
+        Serial.begin(9600); 
+        attachInterrupt( digitalPinToInterrupt(2), ServicioBoton, FALLING);
+   } 
+void loop()
+   {   if (n != contador)
+           {   
+            Serial.println("Interrupcion");
+            Serial.println(contador);
+               n = contador ;
+           }
+   }
+void ServicioBoton()
+   {
+       if ( millis() > T0  + 250)
+          {   contador++ ;
+              T0 = millis();
+          }
+    }
